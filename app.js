02508a94140f6b0083d9630905dcda999975dbb0
Guest <liumingchi@gmail.com>
2025-07-10 15:53:10 +0800

    Fix routing paths for index and users in app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index333');
-var usersRouter = require('./routes/usersre');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
